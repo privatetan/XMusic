3a1ce9eaffad48014159903e39b1c591a6f1f372
privatetan <privatetantao@gmail.com>
2026-03-20 16:25:13 +0800

    Initial Commit

---
+++ XMusic/XMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  XMusicApp.swift
+//  XMusic
+//
+//  Created by Galio on 2026/3/20.
+//
+
+import SwiftUI
+
+@main
+struct XMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
